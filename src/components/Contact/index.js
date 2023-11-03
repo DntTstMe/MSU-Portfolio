ece51c6141ff706746ceff2bdb06533c9908846a
Alex Tester <lex.tester93@gmail.com>
2023-11-03 16:05:04 -0400

    Added code for the contact section

---
+++ src/components/Contact/index.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+
+function Contact() {
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if (name === "email") setEmail(value);
+    else if (name === "userName") setUserName(value);
+    else setMessage(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email) || !userName || !message) {
+      setErrorMessage("Please fill in all the required fields correctly.");
+      return;
+    }
+
+    setErrorMessage(""); // Clear error message
+    setUserName("");
+    setMessage("");
+    setEmail("");
+  };
+  return (
+    <section id="reach-out" className="contact">
+      <div className="flex-row">
+        <h2 className="section-title secondary-border">Reach Out</h2>
+      </div>
+
+      <div className="contact-info">
+        <div>
+          <h3>Hello {userName}</h3>
+          <p>Want to get into contact?</p>
+          <address>
+            Gaines, MI <br />
+            P: <a href="tel:810.867.5309">810.867.5309</a>
+            <br />
+            E:{" "}
+            <a href="mailto://lex.tester93@gmail.com">
+            lex.tester93@gmail.com
+            </a>
+          </address>
+          <p>
+            <strong>I'd love to hear your feedback!</strong>
+          </p>
+        </div>
+
+        {/* contact form section  */}
+        <div className="contact-form">
+          <h3>Contact Me</h3>
+          <form className="form">
+            {/* Name */}
+            <label for="contact-name">Your Name</label>
+            <input
+              value={userName}
+              name="userName"
+              onChange={handleInputChange}
+              type="text"
+              id="contact-name"
+              placeholder="Your Name"
+            />
+
+            {/* Email */}
+            <label for="contact-email">Your Email</label>
+            <input
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              type="email"
+              id="contact-email"
+              placeholder="Your Email"
+            />
+
+            {/* Message */}
+            <label for="contact-message">Message</label>
+            <textarea
+              value={message}
+              name="message"
+              onChange={handleInputChange}
+              type="message"
+              id="contact-message"
+              placeholder="Your Message"
+            />
+            <button type="button" onClick={handleFormSubmit}>
+              Submit
+            </button>
+          </form>
+        </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+}
+
+export default Contact;
+
